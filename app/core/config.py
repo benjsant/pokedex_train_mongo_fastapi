ed6015d2f4d92bb2156c0897cf58c47956640856
benjsant <santrisseb@gmail.com>
2025-11-02 12:56:40 +0100

    Fonctionnalité base mongodb (#1)
    * debut fastapi
    
    * base_init
    
    * base pour futur retrodex a ameliorer

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -15,7 +15,6 @@ class Settings(BaseSettings):
             f"@{self.mongo_cluster}/?{self.mongo_options}"
         )
 
-    class Config: 
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
